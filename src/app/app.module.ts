db16600910e338cdd00b7e496f1504a8d2246f0a
Nolan Docherty <sodinokazaki@gmail.com>
2019-06-05 22:31:15 +0100

    add welcome page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PagesComponent } from './app/modules/welcom/pages/pages.component';
+import { WelcomeComponent } from './modules/welcom/pages/welcome/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PagesComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
