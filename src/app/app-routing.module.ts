4d096bacdee4e305795eb6107b276c4d8e70c01d
Nolan Docherty <sodinokazaki@gmail.com>
2019-06-06 06:27:13 +0100

    add in 404 page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {PageNotFoundComponent} from "./modules/404/pages/page-not-found/page-not-found.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',   redirectTo: '/welcome', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PagesComponent } from './app/modules/welcom/pages/pages.component';
-import { WelcomeComponent } from './modules/welcom/pages/welcome/welcome.component';
+import { WelcomeComponent } from './modules/welcome/pages/welcome/welcome.component';
+import {WelcomeModule} from "./modules/welcome/welcome.module";
+import { PageNotFoundComponent } from './modules/404/pages/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PagesComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
+    WelcomeModule,
     AppRoutingModule
   ],
   providers: [],
