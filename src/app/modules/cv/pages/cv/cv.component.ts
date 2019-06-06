578d6e6e68144c279458964e7e80e65ad364f5dd
Nolan Docherty <sodinokazaki@gmail.com>
2019-06-06 22:30:17 +0100

    make a list appear for the cv

--- src/app/modules/cv/pages/cv/cv.component.ts
+++ src/app/modules/cv/pages/cv/cv.component.ts
@@ -7,6 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CvComponent implements OnInit {
 
+  list = [
+    {label: 'First name', field_name: 'first_name'},
+    {label: 'Last name', field_name: 'last_name'},
+    {label: 'Email', field_name: 'email'},
+    {label: 'Phone number', field_name: 'phone_number'},
+    {label: 'Do you live in the uk?', field_name: 'live_in_uk'},
+    {label: 'Git profile', field_name: 'git_profile'},
+    {label: 'Upload CV', field_name: 'cv'},
+    {label: 'Upload Cover Letter', field_name: 'cover_letter'},
+    {label: 'About you', field_name: 'about_you'},
+  ]
+
+
   constructor() { }
 
   ngOnInit() {
