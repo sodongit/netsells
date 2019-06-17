926c53543e7888d67538213c8da546ab990557ad
Nolan Docherty <sodinokazaki@gmail.com>
2019-06-17 23:57:03 +0100

    bug fix to validators

--- src/app/core/validators/gitUrl.ts
+++ src/app/core/validators/gitUrl.ts
@@ -3,10 +3,8 @@ import {FormControl} from '@angular/forms';
 export function GitUrl() {
   return function (control: FormControl) {
     // validate the file type
-    return control.value && control.value.includes('https://github.com/')?
-      null :
-      {
-        gitUrl: true
-      };
+    return control.value && !control.value.includes('https://github.com/') ?
+      {gitUrl: true}:
+    null;
   }
 }
