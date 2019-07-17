a5321e0953fc007bffd4dc9ea1648efe822bb5e4
Nolan Docherty <sodinokazaki@gmail.com>
2019-07-17 21:05:18 +0100

    add header and footer component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,17 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {FileUploadComponent} from "./components/file-upload/file-upload.component";
 import { UploadProgressComponent } from './components/upload-progress/upload-progress.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     FileUploadComponent,
-    UploadProgressComponent
+    UploadProgressComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule,
   ],
-  exports:[
-    FileUploadComponent
+  exports: [
+    FileUploadComponent,
+    HeaderComponent,
+    FooterComponent
   ]
 })
 export class SharedModule { }
